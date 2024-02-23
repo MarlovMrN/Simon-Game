d0abe5b73e16c2d3a7e1d60301e4f60af98675bd
Marlon Griebeler da Hora <marlongriebeler2@gmail.com>
2024-02-23 01:01:19 -0300

    reverting some code for mobile, leaving mobile unnatended x.x

--- game.js
+++ game.js
@@ -13,15 +13,6 @@ $("html").keydown(function () {
   }
 });
 
-$("html").click(function () {
-  if (!gameStarted) {
-    gameStarted = true;
-    setTimeout(() => {
-      nextSequence();
-    }, 500);
-  }
-});
-
 $("div[type=button]").click(function () {
   if (gameStarted) {
     const userChosenColour = $(this).attr("id");
@@ -79,7 +70,7 @@ function checkAnswer(level) {
     $("#level-title").text(
       "Game Over, score: " +
         (gamePattern.length - 1) +
-        ". Press any key or touch the screen to start again."
+        ". Press any key to start again."
     );
 
     startOver();
